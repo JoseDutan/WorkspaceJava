19983b0b5dbe52f889b78d851b6b8e6f6c0f7533
JoseDutan <josedutan86@gmail.com>
2024-03-16 20:45:05 -0500

    RETO 20 COMPLETADO

---
+++ JavaJoseDutan/src/com/krakedev/test/TestContacto.java
@@ -0,0 +1,31 @@
+package com.krakedev.test;
+
+import com.krakedev.evaluacion.Contacto;
+import com.krakedev.evaluacion.Direccion;
+
+public class TestContacto {
+
+	public static void main(String[] args) {
+	    
+
+        // Crear una instancia de Contacto
+        Contacto contacto = new Contacto("1234567890", "Juan", "Pérez");
+
+        // Settear la dirección
+        Direccion Direccion = new Direccion("Cdl del ejercito", "Pasaje 2B");
+        contacto.setDireccion(Direccion);
+
+        // Mostrar información 
+        System.out.println("\nInformación del Contacto:");
+        contacto.imprimir();
+        
+        Contacto contacto1 = new Contacto("9876543210", "Ana", "Gomes");
+
+        // Settear la dirección
+        Direccion Direccion1 = new Direccion("Cdl del ejercito", "Pasaje 2B");
+        contacto1.setDireccion(Direccion1);
+        System.out.println("\nInformación del Contacto (actualizado):");
+        contacto1.imprimir();
+    }
+}
+
